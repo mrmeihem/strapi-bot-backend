2f719c53f0825784df14612923a4c54eaa9ecbf1
Mikhail Karpenko <70774954+mrmeihem@users.noreply.github.com>
2023-03-23 11:05:35 +0300

    new ver

--- src/api/t-post/controllers/t-post.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * t-post controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::t-post.t-post');
--- src/api/t-post/services/t-post.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * t-post service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::t-post.t-post');
